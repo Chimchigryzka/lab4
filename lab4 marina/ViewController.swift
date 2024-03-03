b6d1fb76ebcfd56a867bc5ef626d194a564f25b7
Chimchigryzka <marchenko1725@gmail.com>
2024-03-03 23:03:17 +0600

    lab4

--- lab4 marina/ViewController.swift
+++ lab4 marina/ViewController.swift
@@ -1,19 +1,22 @@
-//
-//  ViewController.swift
-//  lab4 marina
-//
-//  Created by alina ibraimova on 03.03.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    let exchangeRate: Double = 450
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func convertCurrency(_ sender: UIButton) {
+        if let amountText = amountTextField.text, let amount = Double(amountText) {
+            let result = amount * exchangeRate
+            resultLabel.text = "\(amount) тенге = \(result) долларов"
+        } else {
+            resultLabel.text = "Введите сумму в тенге"
+        }
+    }
 }
-
